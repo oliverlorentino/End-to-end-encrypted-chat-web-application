ed89e07cd0ab0b141d1de69a47eaa704fef14a8c
598028060hhc@gmail.com <q1599s@163.com>
2024-04-10 10:42:13 +0800

    test

--- Web/database.py
+++ Web/database.py
@@ -3,7 +3,9 @@ import mysql.connector
 
 
 def get_mysql_connection():
-    cnx = mysql.connector.connect(user='developer0414', password='QRHqrh13927485@',
+    cnx = mysql.connector.connect(user='th000', password='Nt#9t=Lmwcbw',
                                   host='59.110.162.232', database='chatdb',
                                   port='3306')
     return cnx
+
+
