6f56ea4a8b93fe1160cb95f5d25276284a416be8
oliverlorentino <61689461+oliverlorentino@users.noreply.github.com>
2024-04-15 00:42:50 +0800

    Delete Web directory

--- Web/database.py
+++
@@ -1,9 +0,0 @@
-import mysql
-import mysql.connector
-
-
-def get_mysql_connection():
-    cnx = mysql.connector.connect(user='th000', password='Nt#9t=Lmwcbw',
-                                  host='59.110.162.232', database='chatdb',
-                                  port='3306')
-    return cnx
--- Web/proxy.py
+++
@@ -1,4 +0,0 @@
-from waitress import serve
-from app import app
-
-serve(app, host='0.0.0.0', port=61117)
